b4abba18ed4220df9e19f478de2020c45bbb2823
Ian <ianwallen@hotmail.com>
2025-04-17 16:37:14 +0000

    update createcerts.sh so that it properly creates cert

--- createcerts.sh
+++ createcerts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./.env
-sudo mkdir -p /etc/nginx/certs
-sudo openssl genpkey -algorithm RSA -out /etc/nginx/certs/private.key -pkeyopt rsa_keygen_bits:2048
-sudo openssl req -new -key /etc/nginx/certs/private.key -out /etc/nginx/certs/certificate.csr -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=${BASE_HOSTNAME}-443.app.github.dev"
-sudo openssl x509 -req -days 365 -in /etc/nginx/certs/certificate.csr -signkey ./certs/nginx.key -out ./certs/nginx.crt
+sudo openssl genpkey -algorithm RSA -out ./certs/nginx.key -pkeyopt rsa_keygen_bits:2048
+sudo chmod 644 ./certs/nginx.key
+sudo openssl req -new -key ./certs/nginx.key -out ./certs/certificate.csr -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=${BASE_HOSTNAME}-443.app.github.dev"
+sudo openssl x509 -req -days 365 -in ./certs/certificate.csr -signkey ./certs/nginx.key -out ./certs/nginx.crt
